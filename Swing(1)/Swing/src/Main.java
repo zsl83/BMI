ebbf518a0b3ad4bec901ec621a66983b6d3fb4f1
zsl83 <153368672+zsl83@users.noreply.github.com>
2024-05-14 15:10:57 -0500

    create bmi

--- Swing(1)/Swing/src/Main.java
+++ Swing(1)/Swing/src/Main.java
@@ -1,6 +1,5 @@
 
 
-
 /* References
 swing Doc
 فایل کارگاه 2 برای label H
@@ -18,6 +17,7 @@ https://mahmouddehghanpour.com/%D9%86%D9%85%D8%A7%DB%8C%D8%B4-%D8%AA%D8%B5%D9%88
 https://javapro.ir/%D8%AC%D9%84%D8%B3%D9%87-%D8%A8%DB%8C%D8%B3%D8%AA-%D9%88-%D8%B4%D8%B4%D9%85-%D9%86%D9%85%D8%A7%DB%8C%D8%B4-%D8%AA%D8%B5%D9%88%DB%8C%D8%B1-%D8%AF%D8%B1-Swing-
  */
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -25,135 +25,173 @@ import java.awt.event.*;
 public class Main extends JFrame {
     public static void main(String[] args) {
 
+        // create new frame
         JFrame f = new JFrame("Calculate BMI");
 
-        JPanel j = new JPanel(new GridBagLayout());
-
+        // create first panel
+        JPanel firstPanel = new JPanel(new GridBagLayout());
         GridBagConstraints order = new GridBagConstraints();
         order.insets = new Insets(10, 10, 10, 10);
 
-        /* Setting the front page photo*/
-        JLabel label1 = new JLabel();
+        // Setting the front page photo
+        JLabel labelImage = new JLabel();
         ImageIcon image1 = new ImageIcon("BMI3.png");
-        label1.setIcon(image1);
+        labelImage.setIcon(image1);
         order.gridx = 0;
         order.gridy = 0;
         order.gridwidth = 2;
-        j.add(label1, order);
+        firstPanel.add(labelImage, order);
 
-        /* word of weight*/
-        JLabel label2 = new JLabel("Weight(Kg)");
-        label2.setFont(new Font("Arial", Font.PLAIN, 20));
-        label2.setForeground(Color.white);
+        // word of weight
+        JLabel labelWeight = new JLabel("Weight(Kg)");
+        labelWeight.setFont(new Font("Arial", Font.PLAIN, 24));
+        labelWeight.setForeground(Color.white);
         order.gridx = 0;
         order.gridy = 1;
         order.gridwidth = 1;
-        j.add(label2, order);
+        firstPanel.add(labelWeight, order);
 
-        /* word of Height*/
-        JLabel label3 = new JLabel("Height(m)");
-        label3.setFont(new Font("Arial", Font.PLAIN, 21));
-        label3.setForeground(Color.white);
+        // word of Height
+        JLabel labelHeight = new JLabel("Height(m)");
+        labelHeight.setFont(new Font("Arial", Font.PLAIN, 26));
+        labelHeight.setForeground(Color.white);
         order.gridx = 0;
         order.gridy = 2;
         order.gridwidth = 1;
-        j.add(label3, order);
+        firstPanel.add(labelHeight, order);
 
-        /* Icon at the top of the page*/
+        // Icon at the top of the page
         ImageIcon image = new ImageIcon("bmi.png");
         f.setIconImage(image.getImage());
 
-        /*Calculate button*/
-        JButton d = new JButton("Calculate");
+        //Calculate button
+        JButton calc = new JButton("Calculate");
+        calc.setBackground(new Color(50 , 90,120));
+        calc.setForeground(Color.WHITE);
+        calc.setFont(new Font("Arial", Font.PLAIN, 25));
         order.gridx = 1;
         order.gridy = 5;
         order.gridwidth = 2;
-        j.add(d, order);
+        firstPanel.add(calc, order);
 
-        /* textfield of weight*/
-        JTextField t2 = new JTextField(10);
+        // textField of height
+        JTextField tfHeight = new JTextField(8);
+        tfHeight.setFont(new Font("Arial", Font.BOLD, 25));
         order.gridx = 1;
         order.gridy = 2;
-        j.add(t2, order);
+        firstPanel.add(tfHeight, order);
 
-        /* textfield of Height*/
-        JTextField t1 = new JTextField(10);
+        // textField of weight
+        JTextField tfWeight = new JTextField(8);
+        tfWeight.setFont(new Font("Arial", Font.BOLD, 25));
         order.gridx = 1;
         order.gridy = 1;
-        j.add(t1, order);
+        firstPanel.add(tfWeight, order);
 
-        d.addActionListener(new ActionListener() {
+        // Activate the calculate button
+        calc.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                JPanel K = new JPanel();
+                // create second panel
+                JPanel secondPanel = new JPanel();
 
-                /* color and picture of panel k */
-                K.setBackground(new Color(60 , 50 , 100));
-                ImageIcon image3 = new ImageIcon();
-                JLabel label4= new JLabel();
+                // color and picture of second panel
+                secondPanel.setBackground(new Color(60 , 50 , 100));
+                JLabel labelImage2= new JLabel();
                 ImageIcon image4 = new ImageIcon("BMI5.png");
-                label4.setIcon(image4);
-                K.add(label4);
+                labelImage2.setIcon(image4);
+                secondPanel.add(labelImage2);
 
-                JLabel H= new JLabel();
+                // calculate BMI
+                double w = Double.parseDouble(tfWeight.getText());
+                double h = Double.parseDouble(tfHeight.getText());
+                double c = w / (h*h);
 
-                /* calculate BMI */
-                double a = Double.parseDouble(t1.getText());
-                double b = Double.parseDouble(t2.getText());
-                double c = a / (b*b);
-
-                /* write BMI : c , font , color and position  of it*/
+                // write BMI : c , font , color and position  of it
                 JLabel bmi = new JLabel();
-                K.add(bmi);
                 bmi.setText("BMI:  " + String.format("%.1f", c));
                 bmi.setFont(new Font("Arial" ,Font.PLAIN , 30));
                 bmi.setForeground(Color.white);
                 GridBagConstraints order1 = new GridBagConstraints();
                 order1.gridx = 0;
+                order1.gridy = 0;
+                order1.gridwidth = 2;
+                secondPanel.add(bmi , order1);
+
+                //create label for body condition and color and font of it
+                JLabel labelBmi = new JLabel();
+                labelBmi.setForeground(Color.white);
+                labelBmi.setFont(new Font("Arial", Font.PLAIN, 30));
+                order1.gridx = 0;
                 order1.gridy = 1;
-                order1.gridwidth = 1;
-                K.add(bmi , order1);
+                order1.gridwidth = 2;
+                secondPanel.add(labelBmi,order1);
 
-                /* write body condition*/
-                if (c < 16.0)
-                    H.setText("      Underweight (severe thinness)");
-                if (c <= 16.9 && c >= 16.0)
-                    H.setText("      Underweight (Moderate thinness");
-                if ( c <= 18.4 && c >= 17.0)
-                    H.setText("      Underweight (Mild thinness)");
-                if ( c <= 24.9 && c >= 18.5)
-                    H.setText("      Normal range");
-                if ( c <= 29.9 && c >= 25)
-                    H.setText("      Overweight (Pre-obese)");
-                if ( c <= 34.9 && c >= 30)
-                    H.setText("      Obese (Class I)");
-                if ( c <= 35 && c >= 39.9)
-                    H.setText("      Obese (Class II)");
-                if ( c >= 40 )
-                    H.setText("      Obese (Class III)");
+                // write body condition
+                if (w == 0 || h == 0 || (w == 0 && h == 0)){
+                    if (w == 0)
+                        labelBmi.setText(" weight can't be 0 ");
+                    if (h == 0)
+                        labelBmi.setText(" height can't be 0 ");
+                    if (w == 0 && h == 0)
+                        labelBmi.setText(" height can't be 0 \n weight can't be 0");
+                }
+                    else {
+                    if (c < 16.0)
+                        labelBmi.setText("      Underweight (severe thinness)");
+                    if (c <= 16.9 && c >= 16.0)
+                        labelBmi.setText("      Underweight (Moderate thinness");
+                    if (c <= 18.4 && c >= 17.0)
+                        labelBmi.setText("      Underweight (Mild thinness)");
+                    if (c <= 24.9 && c >= 18.5)
+                        labelBmi.setText("      Normal range");
+                    if (c <= 29.9 && c >= 25)
+                        labelBmi.setText("      Overweight (Pre-obese)");
+                    if (c <= 34.9 && c >= 30)
+                        labelBmi.setText("      Obese (Class I)");
+                    if (c >= 39.9 && c <= 35)
+                        labelBmi.setText("      Obese (Class II)");
+                    if (c >= 40)
+                        labelBmi.setText("      Obese (Class III)");
+                }
 
-                /* color and font of body condition*/
-                H.setForeground(Color.white);
-                H.setFont(new Font("Arial", Font.PLAIN, 30));
+                // create back button
+                JButton back = new JButton("برگشت");
+                back.setFont(new Font("Arial", Font.BOLD, 30));
+                back.setFocusable(false);
+                back.setForeground(Color.WHITE);
+                back.setBackground(new Color(80,90,150));
+                order1.gridx = 0;
+                order1.gridy = 2;
+                order1.gridwidth =2;
+                secondPanel.add(back,order1);
 
+                // Activate the back button
+                back.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        f.remove(secondPanel);
+                        f.add(firstPanel);
+                        f.revalidate();
+                        f.repaint();
+                    }
+                });
 
-
-                K.setBounds(j.getBounds());
-                K.add(H);
-                f.remove(j);
-                f.add(K);
+                secondPanel.setBounds(firstPanel.getBounds());
+                f.remove(firstPanel);
+                f.add(secondPanel);
                 f.revalidate();
                 f.repaint();
             }
         });
 
-        /* color , size and settings of first page (panel j)*/
-        j.setBackground(new Color(30, 85, 100));
+        //color , size and settings of first page
+        firstPanel.setBackground(new Color(30, 85, 100));
         f.setSize(500, 500);
         f.setResizable(false);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(j);
+        f.add(firstPanel);
         f.setVisible(true);
 
     }
